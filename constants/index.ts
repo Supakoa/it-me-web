530355dfdb4051c183e7dfbf0045e2e56eb57969
supakit kitjanabumrungsak <supakoa123@gmail.com>
2022-10-23 22:06:41 +0700

    [EDIT] [skill].ts qeury collection

---
+++ constants/index.ts
@@ -0,0 +1,1 @@
+export const DB_NAME_SKILLS: string = 'skills';
--- pages/api/skills/[skill].ts
+++ pages/api/skills/[skill].ts
@@ -1,3 +1,18 @@
 import { NextApiResponse, NextApiRequest } from 'next';
+import { connectToDatabase } from '@database/connection';
+import { DB_NAME_SKILLS } from 'constants/';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { skill } = req.query;
+  try {
+    const { database } = await connectToDatabase();
+    const collection = database?.collection(DB_NAME_SKILLS);
+    const rs = await collection?.findOne({ skill });
+    return res.json({ ...rs });
+  } catch (e) {
+    console.error(e);
+    return res.json({
+      error: e,
+    });
+  }
+}
